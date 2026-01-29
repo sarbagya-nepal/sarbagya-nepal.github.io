c4c2dce30cd4ac35767ed041c7f4464e456332a0
sarbagya-nepal <sarbagyanpl@gmail.com>
2026-01-29 23:17:43 +0545

    making the portfolio responsive

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("hello world");
+
+let menu = document.querySelector("#hamburger");
+let nav = document.querySelector("nav");
+let toggle = document.querySelector("#toggle");
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("darkmode");
+});
+
+menu.addEventListener("click", () => {
+  nav.classList.toggle("open");
+});
+
+document.addEventListener("click", (e) => {
+  if (nav.classList.contains("open") && !menu.contains(e.target)) {
+    nav.classList.remove("open");
+  }
+});
